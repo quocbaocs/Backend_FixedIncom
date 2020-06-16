0bc94318fca936c2347a55fbc92da9b14112bd4d
root <bacancuoc@gmail.com>
2020-06-17 01:45:33 +0700

    Initial commit 2

--- MovieRaterApi/settings.py
+++ MovieRaterApi/settings.py
@@ -92,9 +92,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
