32ef053ae8a1a59314b56a64620d31db6df7ff20
root <bacancuoc@gmail.com>
2020-06-17 01:43:05 +0700

    Initial commit 2

--- MovieRaterApi/settings.py
+++ MovieRaterApi/settings.py
@@ -23,7 +23,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
